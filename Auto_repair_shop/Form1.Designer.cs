7f06e82714effaf389414e619f1e891638b16f26
feedf <feedf@DESKTOP-1LM8GAK>
2021-10-29 00:58:41 +0300

    1 версия

--- Auto_repair_shop/Form1.Designer.cs
+++ Auto_repair_shop/Form1.Designer.cs
@@ -109,7 +109,6 @@ namespace Auto_repair_shop
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Главная";
-            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- Auto_repair_shop/Form2.Designer.cs
+++ Auto_repair_shop/Form2.Designer.cs
@@ -80,6 +80,8 @@ namespace Auto_repair_shop
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Клиенты";
